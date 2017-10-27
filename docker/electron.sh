457d6878d8e33f1e3e1d3f518a77831da3ac4dbb
Lukasz Sielski <lukaszsielski@Lukaszs-MacBook-Air.local>
2017-10-27 22:29:18 +0100

    fix perms

--- docker/electron.sh
+++ docker/electron.sh
@@ -3,6 +3,7 @@ rm -rf ./compose/app/.build/
 docker-compose down
 docker-compose -f ./docker-compose.electron.yaml build --no-cache
 docker-compose -f ./docker-compose.electron.yaml up 
+sudo chmod 777 -R ./compose/app/.build 
 cp ./compose/app/.build/electron/desktop.js ./compose/app/.build/public/main.js
 cp ./compose/app/src/package.json ./compose/app/.build/public
 electron ./compose/app/.build/public
