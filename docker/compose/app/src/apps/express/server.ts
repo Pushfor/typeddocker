e1de8ac4b31a374cbd23c7555ab9ec874c177c18
Łukasz Marek Sielski <lukaszsielski@gmail.com>
2017-11-02 12:32:11 +0000

    dustjs mvp (#11)

--- docker/compose/app/src/apps/express/server.ts
+++ docker/compose/app/src/apps/express/server.ts
@@ -2,6 +2,7 @@ import * as debug from "debug";
 import * as express from "express";
 import * as fs from "fs";
 import * as morgan from "morgan";
+import { setupDustJSTemplates } from "./dustjs";
 import { update } from "./squirrel/index";
 
 const logger = debug("typeddocker.server");
@@ -24,6 +25,8 @@ app.use(express.static("../build/public", {
 app.get("/update/:platform", update);
 
 app.get("/hello", (req, res) => res.send("hi there"));
+
+setupDustJSTemplates(app);
 
 app.listen(PORT, HOST, null, (err) => {
   if (err) {
